8cfae24c48421236e993fa5034743fb9ff658767
Film-Thananchai-dev <366436241001-st@rmutsb.ac.th>
2025-07-25 11:30:02 +0700

    first commit

---
+++ Basic_Python/test.py
@@ -0,0 +1,12 @@
+# สร้าง Dictionnaries
+group_idm = {
+    1 : 'film',
+    2 : 'tam',
+    3 : 'kade',
+    4 : 'nice',
+    5 : 'mai',
+ }
+
+group_idm[6] = 'tum'
+# print(group_idm)
+print(group_idm[4])
